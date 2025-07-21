adcaef2b66621b8cc67e163728020573705d1728
Cupcakes33 <rute930627@gmail.com>
2025-07-22 07:32:33 +0900

    250722[0] - int arr2[] : field arr2 has C-style array type declaration.

---
+++ src/main/java/org/array/arrEx1.java
@@ -0,0 +1,22 @@
+package org.array;
+
+public class arrEx1 {
+    public static void main(String[] args) {
+
+        int[] arr1 = new int[5];
+        arr1 = new int[]{1, 2, 3, 4, 5};
+
+        int arr2[] = new int[]{6,7,8,9,0};
+        // Field 'arr2' has C-style array type declaration.
+
+        for (int i : arr1){
+            System.out.println(i);
+        }
+
+
+        for(int i=0; i<arr2.length; i++){
+            System.out.println(arr2[i]);
+        }
+    }
+
+}
