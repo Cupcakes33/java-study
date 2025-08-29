92a47deb1b107f12a72ca623a6f3c89908419693
Cupcakes33 <rute930627@gmail.com>
2025-08-29 21:25:58 +0900

    250829[0] - generics 의 extends 로 부모 클래스를 넣더라도 자식을 타입 파라미터로 집어넣을수는 없다. Box<Flute> 과 Box<Apple> 은 다른 타입이기 때문이다.

---
+++ src/main/java/org/generics/GenericEx1.java
@@ -0,0 +1,39 @@
+package org.generics;
+
+import java.util.ArrayList;
+
+public class GenericEx1 {
+    static class Flute {
+        String name;
+    }
+
+    static class Apple extends Flute {
+        String name;
+    }
+
+
+    static class Box<T extends Flute> {
+        ArrayList<T> list = new ArrayList<>();
+
+        void add(T item){list.add(item);}
+        T get(int i){return list.get(i);}
+        ArrayList<T> getList(){return list;}
+        int size(){return list.size();}
+
+        @Override
+        public String toString() {
+            return list.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        Box<Flute> FluteBox = new Box<Flute>();
+        Box<Apple> AppleBox = new Box<Apple>();
+//        Box<Apple> fruteBox = new Box<Frute>();
+//        Box<Frute> fruteBox2 = new Box<Apple>();
+        FluteBox.add(new Apple());
+        FluteBox.add(new Flute());
+        AppleBox.add(new Apple());
+//        AppleBox.add(new Frute());
+    }
+}
